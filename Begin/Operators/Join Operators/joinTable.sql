861f7c6fd6277070c4813c49ed84627209180b9d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-01 14:00:02 +0545

    innerJoin imple phase4

--- Begin/Operators/Join Operators/joinTable.sql
+++ Begin/Operators/Join Operators/joinTable.sql
@@ -3,12 +3,6 @@ CREATE TABLE Authors (
     author_name VARCHAR(50),
     country VARCHAR(50)
 );
-INSERT INTO Authors (author_id, author_name, country) VALUES
-('A1', 'Kiran', 'Nepal'),
-('A2', 'Maya', 'India'),
-('A3', 'John', 'USA'),
-('A4', 'Hana', 'Japan');
-INSERT INTO authors VALUES ('A5','Sophia','UK');
 
 CREATE TABLE Books (
     book_id VARCHAR(5) PRIMARY KEY,
@@ -17,21 +11,12 @@ CREATE TABLE Books (
     year_published INT,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-INSERT INTO Books (book_id, title, author_id, year_published) VALUES
-('B101', 'AI Revolution', 'A1', 2023),
-('B102', 'Data Dreams', 'A2', 2022),
-('B103', 'Cloud Horizons', 'A3', 2024),
-('B104', 'Quantum Tales', 'A5', 2025);
 
 CREATE TABLE Libraries (
     library_id VARCHAR(5) PRIMARY KEY,
     library_name VARCHAR(50),
     city VARCHAR(50)
 );
-INSERT INTO Libraries (library_id, library_name, city) VALUES
-('L1', 'Knowledge Hub', 'Pokhara'),
-('L2', 'Wisdom House', 'Kathmandu'),
-('L3', 'Future Reads', 'Delhi');
 
 CREATE TABLE Library_Books (
     library_id VARCHAR(5),
@@ -40,6 +25,21 @@ CREATE TABLE Library_Books (
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
+INSERT INTO Authors (author_id, author_name, country) VALUES
+('A1', 'Kiran', 'Nepal'),
+('A2', 'Maya', 'India'),
+('A3', 'John', 'USA'),
+('A4', 'Hana', 'Japan'),
+('A5','Sophia','UK');
+INSERT INTO Books (book_id, title, author_id, year_published) VALUES
+('B101', 'AI Revolution', 'A1', 2023),
+('B102', 'Data Dreams', 'A2', 2022),
+('B103', 'Cloud Horizons', 'A3', 2024),
+('B104', 'Quantum Tales', 'A5', 2025);
+INSERT INTO Libraries (library_id, library_name, city) VALUES
+('L1', 'Knowledge Hub', 'Pokhara'),
+('L2', 'Wisdom House', 'Kathmandu'),
+('L3', 'Future Reads', 'Delhi');
 INSERT INTO Library_Books (library_id, book_id) VALUES
 ('L1', 'B101'),
 ('L1', 'B102'),
