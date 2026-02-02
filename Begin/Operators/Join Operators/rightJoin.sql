ae19fd104d1e47f1b1b71a942c5cad08dc0284c1
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-02 12:25:16 +0545

    rightJoin init

---
+++ Begin/Operators/Join Operators/rightJoin.sql
@@ -0,0 +1,5 @@
+# RIGHT JOIN (RIGHT OUTER JOIN )
+-- returns all records from the right table(the second table),and the matched records from the left
+-- table(the first table).
+-- If there is no match, the result is NULL on the side of the left table
+-- Syntax: SELECT columns FROM table1 RIGHT JOIN table2 table1.column_name=table2.column_name;
