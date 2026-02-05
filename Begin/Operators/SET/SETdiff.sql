2aecabc600de6dc30f4e79c814f3a6ea38d784ae
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-05 13:36:50 +0545

    except imple phase2 and final change before merge

--- Begin/Operators/SET/SETdiff.sql
+++ Begin/Operators/SET/SETdiff.sql
@@ -10,3 +10,23 @@
  -- Find destinations with  more than 10,000 visitors in Local_tourism but not in international_tourism.
  SELECT destination FROM local_tourism WHERE visitors>10000 EXCEPT 
  SELECT destination FROM international_tourism;
+ -- Find destinations with  more than 10,000 visitors in international_tourism  but not in Local_tourism.
+ SELECT destination FROM  international_tourism WHERE visitors>10000 EXCEPT 
+ SELECT destination FROM  local_tourism;
+ -- Count how many destinations are unique to Local_tourism (not in international)
+ SELECT COUNT(*) AS unique_destination_in_local FROM
+  (SELECT destination FROM local_tourism EXCEPT SELECT destination FROM international_tourism)
+  AS unique_local_tourism;
+ -- Count how many destinations are unique to international_tourism (not in local)
+ SELECT COUNT(*) AS unique_destination_in_international FROM
+  (SELECT destination FROM international_tourism EXCEPT SELECT destination FROM local_tourism)
+  AS unique_international_tourism;
+  -- Find(destination,category) pairs that exit in Local_tourism but not in internatinal_tourism
+  SELECT destination,category FROM local_tourism EXCEPT
+   SELECT destination,category FROM international_tourism;
+  -- Find(destination,category) pairs that exit in International_tourism but not in local_tourism
+  SELECT destination,category FROM international_tourism EXCEPT
+   SELECT destination,category FROM local_tourism;
+
+
+ 
