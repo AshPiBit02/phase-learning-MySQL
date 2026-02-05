342566c8db18223d38df4e3a8ef122567244f470
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-05 13:28:12 +0545

    except imple phase1

--- Begin/Operators/SET/SETdiff.sql
+++ Begin/Operators/SET/SETdiff.sql
@@ -1,3 +1,12 @@
 # EXCEPT(Set Difference)
  -- returns the rows from the first SELECT statement that are not present in the second SELECT statements.
  -- Syntax: SELECT column1,column2,.... FROM table1 EXCEPT SELECT column1,column2,... FROM table2.
+ -- Find destinations that are in Local_Tourism but not in internation_tourism
+ SELECT destination FROM local_tourism EXCEPT SELECT destination FROM international_tourism;
+ -- Find categories that exits in Local_tourism but not in international_tourism
+ SELECT category FROM local_tourism EXCEPT SELECT category FROM international_tourism;
+ -- Find destinations taht extis in international_tourism but not in local_tourism;
+ SELECT destination FROM international_tourism EXCEPT SELECT destination FROM local_tourism;
+ -- Find destinations with  more than 10,000 visitors in Local_tourism but not in international_tourism.
+ SELECT destination FROM local_tourism WHERE visitors>10000 EXCEPT 
+ SELECT destination FROM international_tourism;
