94dcb7a93ceadecde7f0b7e8113b1fbbedb684bb
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-05 13:04:31 +0545

    EXCEPT init

---
+++ Begin/Operators/SET/SETdiff.sql
@@ -0,0 +1,3 @@
+# EXCEPT(Set Difference)
+ -- returns the rows from the first SELECT statement that are not present in the second SELECT statements.
+ -- Syntax: SELECT column1,column2,.... FROM table1 EXCEPT SELECT column1,column2,... FROM table2.
