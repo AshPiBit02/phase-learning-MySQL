09298a14f824e7119fc0d6fe2d13731a6782cfbb
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-06 14:22:22 +0545

    views imple phase2

--- Begin/Views/demo1.sql
+++ Begin/Views/demo1.sql
@@ -34,3 +34,38 @@ CREATE VIEW emp_finance_above_60000 AS SELECT emp_id,emp_name
  CREATE VIEW emp_dept_loc AS SELECT e.emp_id,e.emp_name,e.department,d.location FROM employees
  e INNER JOIN departments d ON e.department=d.dept_name;
  SELECT * FROM emp_dept_loc;
+ -- Create a view to show employees whose names start with 'S'
+ CREATE VIEW emp_name_s AS SELECT emp_id,emp_name FROM employees WHERE emp_name LIKE 'S%';
+ SELECT * FROM emp_name_s;
+ -- Create a view to show employees with salary between 40000 and 60000.
+ CREATE VIEW salary_range_10k_60k AS SELECT emp_id,emp_name,salary FROM employees
+  WHERE salary>40000 AND salary<60000;--use 'WHERE salary BETWEEN 40000 AND 60000; when including range is required.
+SELECT * FROM salary_range_10k_60k;
+-- Create a view to show employees along with year of hire.
+CREATE VIEW emp_hire_year AS SELECT emp_id,emp_name,YEAR(hire_date) AS hire_year FROM employees;
+SELECT * FROM emp_hire_year;
+-- Create a view to show department-wise maximum salary.
+CREATE VIEW dept_high_salary AS SELECT department,MAX(salary) AS highest_salary FROM employees GROUP BY department;
+SELECT * FROM dept_high_salary; 
+-- Create a view to show employees with salary less than average salary.
+CREATE VIEW less_than_avg_salary AS SELECT emp_id,emp_name,salary FROM employees
+ WHERE salary <(SELECT AVG(salary) FROM employees );
+SELECT * FROM less_than_avg_salary;
+-- Create a view to show employees along with department name and location using INNER JOIN 
+CREATE VIEW emp_dept_info AS SELECT e.emp_id,e.emp_name,e.department,d.location FROM employees e
+INNER JOIN departments d ON e.department=d.dept_name;
+SELECT * FROM emp_dept_info;
+-- Create a view to show employees along with department name using LEFT JOIN.
+CREATE VIEW emp_dept_infol AS SELECT e.emp_id,e.emp_name,e.department,d.location FROM employees e
+LEFT JOIN departments d ON e.department=d.dept_name;
+SELECT * FROM emp_dept_infol;
+CREATE VIEW emp_before_2021 AS SELECT emp_id,emp_name FROM employees WHERE YEAR(hire_date)<2021;
+SELECT * FROM emp_before_2021;
+-- Create a view to show employees with salary greater than all HR employees.
+CREATE VIEW emp_sal_more_than_HR_dept AS SELECT emp_id,emp_name,salary FROM employees
+WHERE salary >( SELECT MAX(salary) FROM employees WHERE department IN ('HR'));
+SELECT * FROM emp_sal_more_than_HR_dept;
+-- Create a view to show employees with salary equal to the minimun salary in the company.
+CREATE VIEW min_salary_emp AS SELECT emp_id,emp_name,salary FROM employees WHERE 
+salary = (SELECT MIN(salary) FROM employees);
+SELECT * FROM min_salary_emp;
