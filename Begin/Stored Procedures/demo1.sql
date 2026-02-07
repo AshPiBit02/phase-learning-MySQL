f86145e18da29355813be370c58875e248460dfb
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-07 13:13:23 +0545

    final commit before merge

--- Begin/Stored Procedures/demo1.sql
+++ Begin/Stored Procedures/demo1.sql
@@ -26,23 +26,41 @@ CALL order_By_quantity(10);
 -- 6. List customers who bought Rice.
 
 -- 7. Calculate the total sales amount.
+
 -- 8. Find the average price of products ordered.
+
 -- 9. Show orderes placed by customers from ordered.
+
 -- 10. Get all orderes where price < 500.
+
 -- 11. Find the top 5 most expensive orders.
+
 -- 12. Show customers who were placed in 2023.
+
 -- 13. Count how many orders were place in 2023.
+
 -- 14. Find the earliest order date.
+
 -- 15. Show all orders of Coffee.
+
 -- 16. Get the total quantity of Milk sold.
+
 -- 17. Find customers who ordered products in Octorber 2023.
+
 -- 18. Show orders wher city= chitwan and product_name=Juice.
+
 -- 19. Find the maximum quantity ordered in a single order.
+
 -- 20. Show all orders sorted by order_date DESC.
+
 -- 21. Get the sum of sales for each city.
+
 -- 22. Find customers who ordered Butter or Cheese.
+
 -- 23. Show orders where price BETWEEN 200 and 1000.
+
 -- 24. List distinct product names ordered.
+
 -- 25. Find customers who ordered on 2023-12-15.
 
 
