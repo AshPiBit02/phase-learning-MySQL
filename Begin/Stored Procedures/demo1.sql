aae685c79a726ee49b3e0223718b938522e8d325
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-08 09:12:42 +0545

    imple phase next

--- Begin/Stored Procedures/demo1.sql
+++ Begin/Stored Procedures/demo1.sql
@@ -87,27 +87,94 @@ END $$
 DELIMITER ;
 CALL orders_according_to_price(500);
 -- 11. Find the top 5 most expensive orders.
-
+DELIMITER $$
+CREATE PROCEDURE top_n_expensive_orders(IN input_n INT)
+BEGIN
+SELECT * FROM orders ORDER BY price DESC LIMIT input_n;
+END $$
+DELIMITER ;
+CALL top_n_expensive_orders(5);
 
 -- 12. Show customers who were placed in 2023.
+DELIMITER $$
+CREATE PROCEDURE cust_placed_according_to_year(IN input_year INT)
+BEGIN
+    SELECT customer_name 
+    FROM orders 
+    WHERE YEAR(order_date) = input_year;
+END $$
+DELIMITER ;
+CALL cust_placed_according_to_year(2023);
 
 -- 13. Count how many orders were place in 2023.
+DROP PROCEDURE count_order_by_year;
+DELIMITER $$
+CREATE PROCEDURE count_order_by_year(IN input_year INT)
+BEGIN
+SELECT COUNT(order_date) AS count_order FROM orders where YEAR(order_date)=input_year GROUP BY YEAR(order_date);
+END $$
+DELIMITER ;
+CALL count_order_by_year(2023);
 
 -- 14. Find the earliest order date.
+DELIMITER $$
+CREATE PROCEDURE earliest_order_date()
+BEGIN
+SELECT order_date FROM orders WHERE order_date=(SELECT MIN(order_date) FROM orders);
+END $$
+DELIMITER ;
+CALL earliest_order_date();
 
 -- 15. Show all orders of Coffee.
 
 -- 16. Get the total quantity of Milk sold.
+DROP PROCEDURE total_quantity_by_product;
+DELIMITER $$
+CREATE PROCEDURE total_quantity_by_product(IN input_prod VARCHAR(50))
+BEGIN
+SELECT SUM(quantity) AS count_quantity FROM orders WHERE product_name=input_prod;
+END $$
+DELIMITER ;
+CALL total_quantity_by_product('Milk');
 
 -- 17. Find customers who ordered products in Octorber 2023.
 
--- 18. Show orders wher city= chitwan and product_name=Juice.
+
+-- 18. Show orders wher city= Chitwan and product_name=Juice.
+DELIMITER $$
+CREATE PROCEDURE order_by_city_product(IN input_city VARCHAR(50),IN input_product VARCHAR(50))
+BEGIN
+SELECT * FROM orders WHERE city=input_city AND product_name=input_product;
+END $$
+DELIMITER ;
+CALL order_by_city_product('Chitwan','Juice');
 
 -- 19. Find the maximum quantity ordered in a single order.
+DELIMITER $$
+CREATE PROCEDURE max_quantity()
+BEGIN
+SELECT * FROM orders WHERE quantity=(SELECT MAX(quantity) FROM orders);
+END $$
+DELIMITER ;
+CALL max_quantity();
 
 -- 20. Show all orders sorted by order_date DESC.
+DELIMITER $$
+CREATE PROCEDURE sort_in_DESC_by_date()
+BEGIN
+SELECT * FROM orders ORDER BY order_date DESC;
+END $$
+DELIMITER ;
+CALL sort_in_DESC_by_date();
 
 -- 21. Get the sum of sales for each city.
+DELIMITER $$
+CREATE PROCEDURE 
+BEGIN
+SELECT
+END $$
+DELIMITER ;
+CALL
 
 -- 22. Find customers who ordered Butter or Cheese.
 
