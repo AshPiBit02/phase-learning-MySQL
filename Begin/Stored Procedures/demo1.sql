cd268b5e4dbf05908d7af9b484a14fd0966e2d0f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-08 08:45:09 +0545

    imple phase continued

--- Begin/Stored Procedures/demo1.sql
+++ Begin/Stored Procedures/demo1.sql
@@ -36,18 +36,58 @@ DELIMITER ;
 CALL order_by_date('2023-09-03');
 
 -- 5. Find the highest priced product ordered.
-
+DELIMITER $$
+CREATE PROCEDURE highest_product()
+BEGIN
+SELECT * FROM orders WHERE price=(SELECT MAX(price) FROM orders);
+END $$
+DELIMITER ;
+CALL highest_product();
 -- 6. List customers who bought Rice.
+DELIMITER $$
+CREATE PROCEDURE customer_by_product(IN prod_name VARCHAR(20) )
+BEGIN 
+SELECT * FROM orders WHERE product_name=prod_name;
+END $$
+DELIMITER ;
+CALL customer_by_product('Rice');
 
 -- 7. Calculate the total sales amount.
+DELIMITER $$
+CREATE PROCEDURE total_sale()
+BEGIN
+SELECT SUM(quantity*price) AS total_trade FROM orders;
+END $$
+DELIMITER;
+CALL total_sale();
 
 -- 8. Find the average price of products ordered.
-
+DELIMITER $$
+CREATE PROCEDURE avg_price_of_prod()
+BEGIN
+SELECT AVG(price) AS avg_price_of_product FROM orders;
+END $$
+DELIMITER ;
+CALL avg_price_of_prod();
 -- 9. Show orderes placed by customers from ordered.
+DELIMITER $$
+CREATE PROCEDURE 
+BEGIN
+SELECT
+END $$
+DELIMITER ;
+CALL
 
 -- 10. Get all orderes where price < 500.
-
+DELIMITER $$
+CREATE PROCEDURE orders_according_to_price(IN input_price INT)
+BEGIN
+SELECT * FROM orders WHERE price<input_price;
+END $$
+DELIMITER ;
+CALL orders_according_to_price(500);
 -- 11. Find the top 5 most expensive orders.
+
 
 -- 12. Show customers who were placed in 2023.
 
