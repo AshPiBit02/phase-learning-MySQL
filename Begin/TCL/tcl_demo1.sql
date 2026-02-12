7e1be64f4dcd8fcf6fdef24fb39c2fc74abc499d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-02-12 10:44:47 +0545

    tcl imple phase2

--- Begin/TCL/tcl_demo1.sql
+++ Begin/TCL/tcl_demo1.sql
@@ -37,7 +37,35 @@ INSERT INTO banktransactions(account_no,txn_type,amount, txn_date)
  SELECT * FROM banktransactions;
  COMMIT;
  SELECT * FROM banktransactions;
+-- 7. Insert a deposite for ACC1012 =5000.00, set SAVEPOINT sq1, then insert 
+-- another deposite for ACC1013=4500.00, and ROLLBACK to sp1
+START TRANSACTION;
+ INSERT INTO banktransactions(account_no,txn_type,amount, txn_date)
+ VALUES('ACC1010','WITHDRAWAL',5000.00,NOW());
+ SELECT * FROM banktransactions;
+ ROLLBACK;
+ SAVEPOINT sp1;
+ INSERT INTO banktransactions(account_no,txn_type,amount, txn_date)
+ VALUES('ACC1010','WITHDRAWAL',800.00,NOW()),('ACC1011','DESPOSITE',600.00,NOW());
+ SELECT * FROM banktransactions;
+ ROLLBACK TO sp1;
+ SELECT * FROM banktransactions;
+--  8. Update txn_id =4 to amount 2000.00, set SAVEPOINT sp2, then rollback to sp2
+BEGIN;
+SAVEPOINT sp2;
+UPDATE banktransactions SET amount=2000.00 WHERE txn_id=4;
+SELECT * FROM banktransactions;
+ROLLBACK TO sp2;
+SELECT * FROM banktransactions;
 
+-- 9. Use SET TRANSACTION ISOLATION LEVEL SERIALIZABLE and insert a withdrawal for ACC1010=500.00 observe stricter isolation
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED; -- ensures that within this transaction,user can only read rows that have already committed by  other transactions.
+BEGIN;
+INSERT INTO banktransactions(account_no,txn_type,amount,txn_date)
+ VALUES('ACC1010','DEPOSITE',500.00,NOW());
+
+ SELECT * FROM banktransactions;
+ COMMIT;
 
 
 
